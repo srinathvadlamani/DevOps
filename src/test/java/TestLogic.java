ea624437a281288c84171a0b83e897e890c45159
Yamini  Vishtnubhatta <1238674@TCS.com>
2018-11-09 09:55:32 +0530

    DevOps Project

---
+++ src/test/java/TestLogic.java
@@ -0,0 +1,12 @@
+  
+ import static org.junit.Assert.*;  
+  
+import org.junit.Test;  
+  
+public class TestLogic {  
+  
+    @Test  
+    public void testFindMax(){  
+     assertTrue(false);
+    }  
+}  
